d0152d341e3b87ad6cde5f554069c273601266b5
Belbouab <Belbouab.Younes@gmail.com>
2024-10-15 10:25:53 +0200

    Adding LLM chatbot

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from mistralai import Mistral
+
+def generate_response(user_input):
+    api_key = "B9I8qboZhE42PjvnGkxoctSgWBFmgwTa"
+    #regardez mon readme Spec2match pour la clé API
+    model = "mistral-large-latest"
+
+    client = Mistral(api_key=api_key)
+
+    chat_response = client.chat.complete(
+        model=model,
+        messages=[
+            {
+                "role": "user",
+                "content": user_input,
+            },
+        ]
+    )
+
+    return chat_response.choices[0].message.content
+
+st.title("Chatbot avec Streamlit")
+st.write("Bienvenue sur l'interface de chatbot. Posez-moi des questions !")
+
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = [{
+                "content": "Parle comme un empereur Romain",
+            },]
+
+with st.form(key='chat_form', clear_on_submit=True):
+    user_input = st.text_input("Vous :", key="input")
+    submit_button = st.form_submit_button(label='Envoyer')
+
+if submit_button and user_input:
+    response = generate_response(user_input)
+    st.session_state.chat_history.append(("Vous", user_input))
+    st.session_state.chat_history.append(("Bot", response))
+
+for sender, message in st.session_state.chat_history:
+    if sender == "Vous":
+        st.write(f"**{sender}:** {message}")
+    else:
+        st.write(f"*{sender}:* {message}")
+
